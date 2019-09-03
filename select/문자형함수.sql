f74146e2670ae461a2ae79f6eb6b65a936e57a92
Junho Park <jhpark870708@daum.net>
2019-09-03 15:47:54 +0900

    SQL 연습 01

---
+++ select/문자형함수.sql
@@ -0,0 +1,40 @@
+-- upper
+-- 1. 자바 upperCase보다 DB의 upper() 함수가 훨씬 빠르다.
+-- 2. 웬만하면 DB에서 문자열 처리뿐만 아니라 포맷팅 처리 등을 다 해주고
+-- 자바에서 출력만 해결한다.
+-- 3. 자바 코드가 간결해서 좋다.
+
+select upper('SeouL'), ucase('seoul');
+select upper(first_name) from employees;
+
+select first_name from employees;
+
+-- lower
+select lower('SEoul'), lcase('SEOUL');
+
+-- substring() : DB는 index가 무조건 1부터 시작한다.
+select substring('Happy Day', 3, 2);
+
+select first_name as '이름', substring(hire_date, 1, 4) as '입사년도' from employees;
+
+-- lpad, rpad : 정렬
+select lpad('1234', 10, '-');
+select rpad('1234', 10, '-');
+ 
+-- ex1) salaries 테이블에서 2001년 급여가 70000불 이하의 직원만 사번, 급여로 출력하되 급여는 10자리로 부족한 자리수는 *로 표시
+select emp_no, lpad(cast(salary as char), 10, '*') from salaries 
+where from_date like '2001%' and salary < 70000;
+
+-- ltrim, rtrim. trim
+select concat('---', ltrim('      hello      '), '---') as 'LTRIM',
+       concat('---', rtrim('      hello  xxx '), '---') as 'RTRIM',
+       concat('---', trim('      hello      '), '---') as 'TRIM',
+	   concat('---', trim(both 'x' from 'xxxxxxhelloxxxxxx'), '---') as 'TRIM2',
+       concat('---', trim(leading 'x' from 'xxxxxxhelloxxxxxx'), '---') as 'TRIM3',
+       concat('---', trim(trailing 'x' from 'xxxxxxhelloxxxxxx'), '---') as 'TRIM4';
+
+
+
+
+
+
